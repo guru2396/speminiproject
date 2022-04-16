e50e3e71a1a03b99873a5411e17f448323a6562e
guru2396 <gururajm2396@gmail.com>
2022-04-16 09:56:17 +0530

    ansible changes

--- src/test/java/ScientificCalculatorTest.java
+++ src/test/java/ScientificCalculatorTest.java
@@ -8,7 +8,9 @@ public class ScientificCalculatorTest {
         ScientificCalculator scientificCalculator=new ScientificCalculator();
         double actual=scientificCalculator.squareroot(25.0);
         double expected=5.0;
+        double NotExpected=6.0;
         Assertions.assertEquals(expected,actual);
+        Assertions.assertNotEquals(NotExpected,actual);
     }
 
     @Test
@@ -17,7 +19,9 @@ public class ScientificCalculatorTest {
         double num=145.256;
         double actual= scientificCalculator.naturalLog(num);
         double expected=4.978497702968366;
+        double NotExpected=6.0;
         Assertions.assertEquals(expected,actual);
+        Assertions.assertNotEquals(NotExpected,actual);
     }
 
     @Test
@@ -25,7 +29,9 @@ public class ScientificCalculatorTest {
         ScientificCalculator scientificCalculator=new ScientificCalculator();
         int actual= scientificCalculator.factorial(5);
         int expected=120;
+        int notExpected=720;
         Assertions.assertEquals(expected,actual);
+        Assertions.assertNotEquals(notExpected,actual);
     }
 
     @Test
@@ -34,6 +40,8 @@ public class ScientificCalculatorTest {
         double num=3.0;
         double actual=scientificCalculator.power(num,num);
         double expected=27.0;
+        double notExpected=64.0;
         Assertions.assertEquals(expected,actual);
+        Assertions.assertNotEquals(notExpected,actual);
     }
 }
